61e94931e8cd61b92c3f076884f0ca9d53411d3c
Deiri <66674076+Deiri@users.noreply.github.com>
2020-09-29 20:17:02 +0300

    Add files via upload

---
+++ check.py
@@ -0,0 +1,58 @@
+
+import sys
+import os 
+import random
+ 
+
+
+os.system('clear')
+print('╔═══╗╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔═══╗╔═══╗')
+print('║╔═╗║║║─║║║╔══╝║╔═╗║║║║╔╝║╔══╝║╔═╗║')
+print('║║─╚╝║╚═╝║║╚══╗║║─╚╝║╚╝╝─║╚══╗║╚═╝║')
+print('║║─╔╗║╔═╗║║╔══╝║║─╔╗║╔╗║─║╔══╝║╔╗╔╝')
+print('║╚═╝║║║─║║║╚══╗║╚═╝║║║║╚╗║╚══╗║║║╚╗')
+print('╚═══╝╚╝─╚╝╚═══╝╚═══╝╚╝╚═╝╚═══╝╚╝╚═╝')
+print("")
+print("by Deiri  v.1")
+print("")
+print("")
+print ('Начать поиск по базам данных?:')
+print ("")
+print ("[1] Да")
+print ("[2] Нет")
+print ("")
+open=input("Введите цифру : ")
+os.system('clear')
+print('╔═══╗╔╗─╔╗╔═══╗╔═══╗╔╗╔═╗╔═══╗╔═══╗')
+print('║╔═╗║║║─║║║╔══╝║╔═╗║║║║╔╝║╔══╝║╔═╗║')
+print('║║─╚╝║╚═╝║║╚══╗║║─╚╝║╚╝╝─║╚══╗║╚═╝║')
+print('║║─╔╗║╔═╗║║╔══╝║║─╔╗║╔╗║─║╔══╝║╔╗╔╝')
+print('║╚═╝║║║─║║║╚══╗║╚═╝║║║║╚╗║╚══╗║║║╚╗')
+print('╚═══╝╚╝─╚╝╚═══╝╚═══╝╚╝╚═╝╚═══╝╚╝╚═╝')
+print("")
+print("by Deiri  v.1")
+print("")
+if(open=='1'):
+    print ("")
+    d=print('Напишите mail/gmail/phone:')
+    print("")
+    a=input('Логин : ')
+    j=['qwerty123' , 'vanya228' , 'logovo123' , 'sex1980' , 'password' , '1234567' , 'йцукен123' , '7654321' , '123qwe' , 'klaster' , 'qazxsw' , '1q2w3e4r' , '1qaz2wsx' , '1q2w3e4r5t' , 'qwertyuiop' , '1234qwer' , '123QWE123' , 'brawlstars' , 'leon1234' , 'spike334' , 'crow1234' , 'kopik123' , 'cheburek' , 'geniy' , 'line12']
+    h=print("Пароль -" , random.choice(j))
+    print("")
+    print("Нажмите CTRL + C чтобы выйти")
+    g=input("")
+    l=input("")
+    p=input("")
+    x=input("")
+    gl=input("")
+    ll=input("")
+    pl=input("")
+    xk=input("")
+    gk=input("")
+    lk=input("")
+    pk=input("")
+    xl=input("")
+else:
+    d='Нажмите CTRL + C чтобы выйти'
+print(d)
